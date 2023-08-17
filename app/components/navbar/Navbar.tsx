de9374e65150871a6a9df9d5b40fc7a282b96ef4
abu sayed <romanyad16@gmail.com>
2023-08-17 16:34:29 +0800

    Ready to Publish

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,24 @@
+'use client';
+
+import Container from '../Container';
+import Search from './Search';
+import UserMenu from './UserMenu';
+import Logo from './logo';
+
 const Navbar = () => {
-  return <div>I am a Navbar</div>;
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
