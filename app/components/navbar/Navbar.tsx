b7c73678911cc3a8404d1bffc1bef27586ab6492
Abu Sayed <76618493+romanyad@users.noreply.github.com>
2023-08-18 04:10:11 +0800

    Update Navbar.tsx

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,9 +2,9 @@ import { SafeUser } from "@/app/types";
 
 import Categories from "./Categories";
 import Container from "../Container";
-import Logo from './logo';
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import Logo from './logo';
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
